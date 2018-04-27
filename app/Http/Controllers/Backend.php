2ccb9ba7034e9cbd2cc3154e6beba50261afd872
unknown <shresthaneeraj94@gmail.com>
2018-04-27 14:47:01 +0545

    add event completed

--- app/Http/Controllers/Backend.php
+++ app/Http/Controllers/Backend.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\event;
 use App\image;
 use Illuminate\Http\Request;
 
@@ -20,7 +21,31 @@ class Backend extends Controller
     }
     public function addEventAction(Request $request)
     {
-        return $request;
+        $this->validate($request, [
+            'title' => 'required',
+            'location'=>'required',
+            'starting_date'=>'required',
+            'ending_date'=>'required',
+            'image' => 'required',
+            'details' => 'required'
+        ]);
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $extension = strtolower($file->extension());
+            $newName = time() . '_.' . $extension;
+            $request->image->move(public_path('image/uploads/events'), $newName);
+            $data['image'] = $newName;
+        }
+
+        $data['title']=$request->title;
+        $data['location']=$request->location;
+        $data['starting_date']=$request->starting_date;
+        $data['ending_date']=$request->ending_date;
+        $data['details']=htmlspecialchars($request->details);
+        if (event::create($data)) {
+            return redirect()->route('add-event')->with('success', 'Event added successfully');
+        }
+        return redirect()->back()->with('fail', 'Problem encounter while adding event');
     }
     public function addImage()
     {
@@ -30,8 +55,6 @@ class Backend extends Controller
     {
         $this->validate($request, [
             'title' => 'required',
-            'location'=>'required',
-            'image' => 'required',
             'description' => 'required'
         ]);
         if ($request->hasFile('image')) {

52de05cfbc96a14ed80cefa822da3f391b78dedf
b7Rexx <bij.maharjan@gmail.com>
2018-04-27 11:17:02 +0545

    events

--- resources/views/Frontend/pages/event.blade.php
+++ resources/views/Frontend/pages/event.blade.php
@@ -5,5 +5,32 @@
 @endsection
 
 @section('body')
-    <h1>Event</h1>
+    <div class="event_block">
+        <div class="container">
+            <br>
+            <h1 class="text-center">Event</h1>
+            @for($i=0;$i<5;$i++)
+                <hr>
+                <div class="event">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="{{URL::to('image/uploads/gallery/1.jpg')}}" alt="photo">
+                        </div>
+                        <div class="col-md-6">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, laborum libero nulla
+                                pariatur
+                                perferendis sunt velit vero! Ad commodi consequuntur dolorem esse inventore natus neque
+                                odit
+                                quisquam. Ducimus, porro repellendus.</p>
+                            <a href="#">See more ... </a>
+                        </div>
+                        <div class="col-md-3">
+                            {{date('Y/m/d')}}
+                        </div>
+                    </div>
+                </div>
+
+            @endfor
+        </div>
+    </div>
 @endsection
